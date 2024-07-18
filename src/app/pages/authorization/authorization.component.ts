ae01bbf1053e35569913a9039bf6b13cd310fe4b
djufer <djufer@ukr.net>
2024-07-18 19:24:59 +0300

    зробив subscribe по новому

--- src/app/pages/authorization/authorization.component.ts
+++ src/app/pages/authorization/authorization.component.ts
@@ -1,15 +1,10 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
-import {
-  Auth,
-  signInWithEmailAndPassword,
-  UserCredential,
-} from '@angular/fire/auth';
+import { Auth, signInWithEmailAndPassword } from '@angular/fire/auth';
 import { docData, doc, Firestore } from '@angular/fire/firestore';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { Subscription } from 'rxjs';
 import { ROLE } from 'src/app/shared/constants/role.constant';
-import { AccountService } from 'src/app/shared/services/account/account.service';
 
 @Component({
   selector: 'app-authorization',
@@ -23,7 +18,6 @@ export class AuthorizationComponent implements OnInit, OnDestroy {
 
   constructor(
     private fb: FormBuilder,
-    private accountService: AccountService,
     private router: Router,
     private auth: Auth,
     private afs: Firestore
@@ -32,6 +26,7 @@ export class AuthorizationComponent implements OnInit, OnDestroy {
   ngOnInit(): void {
     this.initAuthForm();
   }
+
   ngOnDestroy(): void {
     this.loginSubscription.unsubscribe();
   }
@@ -44,20 +39,6 @@ export class AuthorizationComponent implements OnInit, OnDestroy {
   }
 
   loginUser(): void {
-    // this.accountService.login(this.authForm.value).subscribe(data => {
-    //   if (data && data.length > 0) {
-    //     const user = data[0];
-    //     localStorage.setItem('currentUser', JSON.stringify(user));
-    //     this.accountService.isUserLogin$.next(true);
-    //     if (user && user.role === ROLE.USER) {
-    //       this.router.navigate(['/cabinet'])
-    //     } else if (user && user.role === ROLE.ADMIN) {
-    //       this.router.navigate(['/admin'])
-    //     }
-    //   }
-    // }), (e: any) => {
-    //   console.log(e);
-    // }
     const { email, password } = this.authForm.value;
     this.login(email, password)
       .then(() => {
@@ -77,14 +58,19 @@ export class AuthorizationComponent implements OnInit, OnDestroy {
     console.log(credential.user.uid);
     this.loginSubscription = docData(
       doc(this.afs, 'users', credential.user.uid)
-    ).subscribe(
-      user => {
+    ).subscribe({
+      next: (user) => {
         const currentUser = { ...user, uid: credential.user.uid };
         localStorage.setItem('currentUser', JSON.stringify(currentUser));
+        if (user && user['role'] === ROLE.USER) {
+          this.router.navigate(['/cabinet']);
+        } else if (user && user['role'] === ROLE.ADMIN) {
+          this.router.navigate(['/admin']);
+        }
       },
-      (e) => {
-        console.log('fuck error', e);
-      }
-    );
+      error: (e) => {
+        console.log('error', e);
+      },
+    });
   }
 }
